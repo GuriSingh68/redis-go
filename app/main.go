8b7212cc4af939a38bd34b26a611cd4c6e781ea7
GuriSingh68 <141493117+GuriSingh68@users.noreply.github.com>
2025-07-19 21:51:57 -0400

    echo command

--- app/main.go
+++ app/main.go
@@ -15,7 +15,7 @@ var _ = os.Exit
 func main() {
 	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
-		panic(err)
+		os.Exit(1)
 	}
 
 	for {
@@ -36,6 +36,27 @@ func handleConnection(conn net.Conn) {
 		text := scanner.Text()
 		if strings.TrimSpace(text) == "PING" {
 			conn.Write([]byte("+PONG\r\n"))
+		} else if strings.Contains(strings.ToUpper(text), "ECHO") {
+			// Extract the message after ECHO
+			lines := strings.Split(text, "\n")
+			var message string
+			echoFound := false
+
+			for _, line := range lines {
+				line = strings.TrimSpace(line)
+				if echoFound && line != "" && !strings.HasPrefix(line, "$") {
+					message = line
+					break
+				}
+				if strings.ToUpper(line) == "ECHO" {
+					echoFound = true
+				}
+			}
+
+			if message != "" {
+				response := fmt.Sprintf("$%d\r\n%s\r\n", len(message), message)
+				conn.Write([]byte(response))
+			}
 		}
 
 	}
